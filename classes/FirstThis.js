04dee7dbee5905828374de56a8d950b38eea122b
Mike <mike@gamefromscratch.com>
2012-07-24 21:29:34 -0400

    tada

--- classes/FirstThis.js
+++ classes/FirstThis.js
@@ -2,8 +2,18 @@
 var FirstThis = cc.LayerColor.extend({
     leftSprite:null,
     rightSprite:null,
+    mode:0,
     imageChanged:function(imgName,whichSprite){
+        this.mode=0;
         this.removeAllChildrenWithCleanup(true);
+
+        if(this.leftSprite != null && whichSprite=="right")
+        {
+            this.addChild(this.leftSprite);
+        }
+        if(this.rightSprite != null && whichSprite=="left"){
+            this.addChild(this.rightSprite);
+        }
 
         var imageSize;
         YUI().use('node','io-base',function(Y){
@@ -11,41 +21,32 @@ var FirstThis = cc.LayerColor.extend({
             imageSize = JSON.parse(results.responseText);
         });
 
-        var sprite = cc.Sprite.create("/image/" + imgName);//, new cc.Rect(0,0,imageSize.width,imageSize.height));
-        sprite.touchesEnded()
-        var widthOffset;
-        var windowWidth = window.innerWidth;
-        var windowHeight = window.innerHeight;
-
-        var newSpriteWidth = windowWidth/2;
-        var newSpriteHeight = windowHeight/2;
+        var newSpriteWidth = cc.Director.sharedDirector().getWinSize().width/2;
+        var newSpriteHeight = cc.Director.sharedDirector().getWinSize().height/2;
 
         if(whichSprite == "left"){
-            leftSprite = sprite;
-            widthOffset = 0;
+            this.leftSprite = cc.Sprite.create("/image/" + imgName,new cc.Rect(0,0,imageSize.width,imageSize.height));
+            this.addChild(this.leftSprite);
+            this.leftSprite.setScale((newSpriteWidth * this.leftSprite.getScaleX())/imageSize.width);
+            this.leftSprite.setAnchorPoint(0,1);
+            this.leftSprite.setPosition(new cc.Point(0,0));
         }
         else
         {
-            rightSprite = sprite;
-            widthOffset = windowWidth/2;
+            this.rightSprite = cc.Sprite.create("/image/" + imgName, new cc.Rect(0,0,imageSize.width,imageSize.height));
+            this.addChild(this.rightSprite);
+            this.rightSprite.setScale((newSpriteWidth * this.rightSprite.getScaleX())/imageSize.width);
+            this.rightSprite.setAnchorPoint(0,1);
+            this.rightSprite.setPosition(new cc.Point(newSpriteWidth,0));
         }
-        sprite.setAnchorPoint(0,0);
-
-
-        if(typeof(leftSprite)=="object")
-            this.addChild(leftSprite);
-        if(typeof(rightSprite)=="object")
-            this.addChild(rightSprite);
-
-
-        sprite.setScale((newSpriteWidth * sprite.getScaleX())/imageSize.width);
-        sprite.setAnchorPoint(new cc.ccp(0.0,1.0));
-        sprite.setPosition(new cc.Point(0 + widthOffset,cc.Director.sharedDirector().getWinSize().height));
-
     },
-
+    ctor:function()
+    {
+        this._super();
+    },
     init:function()
     {
+        this.setIsTouchEnabled(true);
         this.initWithColor(cc.ccc4(0,0,0,255));
 
         var that = this;
@@ -62,6 +63,32 @@ var FirstThis = cc.LayerColor.extend({
         });
         this.setAnchorPoint(0,0);
         return this;
+    },
+    ccTouchesEnded:function (pTouch,pEvent){
+        if(this.leftSprite != null && this.rightSprite != null ){
+            this.mode++;
+            if(this.mode == 1)
+            {
+                //this.leftSprite.setScale((cc.Director.sharedDirector().getWinSize().width * this.leftSprite.getScaleX())/this.leftSprite.getContentSize().width);
+                this.leftSprite.setIsVisible(true);
+                this.rightSprite.setIsVisible(false);
+                this.leftSprite.setPosition(new cc.Point(cc.Director.sharedDirector().getWinSize().width/4,0));
+            }
+            else if(this.mode == 2)
+            {
+                this.leftSprite.setIsVisible(false);
+                this.rightSprite.setIsVisible(true);
+                this.rightSprite.setPosition(new cc.Point(cc.Director.sharedDirector().getWinSize().width/4,0));
+            }
+            else{
+                this.leftSprite.setIsVisible(true);
+                this.rightSprite.setIsVisible(true);
+                this.leftSprite.setPosition(new cc.Point(0,0));
+                this.rightSprite.setPosition(new cc.Point(cc.Director.sharedDirector().getWinSize().width/2,0));
+                this.mode = 0;
+            }
+        }
+
     }
 });
 
